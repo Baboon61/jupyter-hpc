1e3865fd6407a132d48cdebdd13857ddda3b77cf
Bastien Hervé <bastienherve61@gmail.com>
2026-04-22 13:53:40 +0200

    Update project information in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'jupyter-hpc'
+copyright = '2026, Bastien Hervé'
+author = 'Bastien Hervé'
 
 release = '0.1'
 version = '0.1.0'
